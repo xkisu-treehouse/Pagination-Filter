4ae72166fdec344da964ab4ab42142d2a110f743
Keith Mitchell <keith.mitchell@live.ca>
2017-09-27 16:26:53 -0700

    Added search functionality

--- app.js
+++ app.js
@@ -1,12 +1,84 @@
 $(document).ready(function() {
-    // when the page is loaded show the first page
-    paginate(0);
+    // when the page is loaded show the first page and add the search bar
+    add_search();
+    /* 
+        search with an empty query to add the search classes to the elements
+        an emtpy string means it matches everything
+    */
+    search("");
 });
+
+// adds the searchbar
+function add_search() {
+    // the element to hold the search input
+    const search_container = $('.page-header.cf');
+    // create the search input element
+    const searchbar = $('<input type="search"></input>');
+    // add a placeholder to give it some context
+    searchbar.attr('placeholder', 'Search...');
+    // event handler for when the search query changes
+    searchbar.keyup(function() {
+        // the search query
+        var query = $(this).val();
+        // call the search function
+        search(query);
+    });
+    // add the searchbar to the page
+    search_container.append(searchbar);
+}
+// runs a search
+function search(query) {
+    // lets use a simple regex to filter the students, the i flag is case-insensitive
+    var re = new RegExp(query, "gi");
+    // get the students
+    const students = $('.student-list').children();
+
+    var matches = 0;
+
+    students.each(function(i, el) {
+        var details = $($(this).children('.student-details').get(0));
+        // get the name from the html element
+        var name = details.children('h3').html();
+        // get the email from the html element
+        var email = details.children('.email').html();
+
+        // check if either match the search
+        if (re.test(name) || re.test(email)) {
+            $(this).addClass('match'); // if it matches add the class
+            ++matches;
+        } else {
+            $(this).removeClass('match'); // if it doesn't match remove it
+        }
+        console.log(name);
+    });
+
+    // remove the no matches text if there is one on the page
+    $('#no_matches').remove();
+
+    // if there are not matches then say "no matches"
+    if (matches == 0) {
+        //the element that will hold the no matches text
+        const container = $('.page');
+        // create a new element to hold the text
+        const text_container = $('<h1>No matches found!</h1>');
+        // set the ID so we can reference it later
+        text_container.attr('id', 'no_matches');
+        // add the text to the page
+        container.append(text_container);
+    }
+
+    // reset the pagination so it only applies to the matched elements
+    paginate(0);
+}
 
 // adds or removes and re-adds the pagination buttons
 function paginate_buttons(page, totalPages) {
     // sanity check - make sure there isn't already buttons on the page
     $('#pagination_buttons').remove();
+
+    // if there are no pages don't create the page button elements
+    if (totalPages == 0)
+        return;
 
     //the element that will hold the button element
     const container = $('.page');
@@ -38,9 +110,14 @@ function paginate_buttons(page, totalPages) {
     container.append(button_container);
 }
 
+// paginates the students
 function paginate(page) {
     // the list of students
-    const students = $('.student-list').children();
+    // the match class is given to elements that matched the search
+    const students = $('.student-list').children('.match');
+
+    // hide all the students
+    $('.student-list').children().hide();
 
     // calculate how many pages of students we have
     const studentsPerPage = 10;
@@ -51,9 +128,6 @@ function paginate(page) {
         page = totalPages - 1;
     if (page < 0)
         page = 0;
-
-    // hide all the students
-    students.hide();
 
     // start and and of the for-loop
     const start = (studentsPerPage * page);
@@ -66,7 +140,6 @@ function paginate(page) {
 
     // display the relevant students 
     for (let i = start; i < end; i++) {
-        //console.log(i);
         var element = students.eq(i);
         element.show(); // show the element
     }
